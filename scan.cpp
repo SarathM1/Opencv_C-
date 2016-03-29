d3c2d90eb996d83d6814c0c2e6787595f7186500
Office PC <sarathm00@gmail.com>
2016-03-29 14:46:06 +0530

    Scan and change all pixel values in an image - 3 methods

--- scan.cpp
+++ scan.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 using namespace std;
 
 
-Mat& scanC(Mat& img, const uchar* const table)
+void scanC(Mat& img, const uchar* const table)
 {
     CV_Assert(img.depth() == CV_8U);
 
@@ -31,9 +31,48 @@ Mat& scanC(Mat& img, const uchar* const table)
         }
     }
 
-    imshow("Final",img);
-    waitKey(0);
-    return img;
+    
+}
+
+void scanIterator(Mat& img,const uchar* const table)
+{
+    int channels = img.channels();
+    switch(channels)
+    {
+        case 1:
+            {
+                MatIterator_<uchar> it,end;
+                for(it = img.begin<uchar>(),end=img.end<uchar>();it!=end;it++)
+                    *it = table[*it];
+                break;
+            }
+        case 3:
+            {
+                MatIterator_<Vec3b> it,end;
+                for(it = img.begin<Vec3b>(), end = img.end<Vec3b>(); it != end; ++it)
+                {
+                    (*it)[0] = table[(*it)[0]];
+                    (*it)[1] = table[(*it)[1]];
+                    (*it)[2] = table[(*it)[2]];
+                }
+                break;
+            }
+    }
+    
+}
+
+Mat scanLUT(Mat &img,const uchar* const table)
+{
+    Mat lookUpTable(1,256,CV_8U);
+
+    uchar *p = lookUpTable.data;
+
+    for(int i=0; i < 256; ++i)
+        p[i] = table[i];
+    
+    Mat img_new;
+    LUT(img,lookUpTable,img_new);
+    return img_new;
 }
 
 int main(int argc,char **argv)
@@ -54,7 +93,21 @@ int main(int argc,char **argv)
     for(int i=0;i<256;i++)
         table[i] = (uchar)(divideWidth*(i/divideWidth));
 
-    Mat img;
+    Mat img,img_1,img_2,img_3;
     img = imread("test.jpg");
-    scanC(img,table);
+    
+    img_1 = img.clone();
+    img_2 = img.clone();
+    img_3 = img.clone();
+
+    scanIterator(img_1,table);
+    scanC(img_2,table);
+    img_3 = scanLUT(img_3,table);
+
+    imshow("ScanC",img_2);
+    imshow("ScanIterator",img_1);
+    imshow("ScanLUT",img_3);
+
+    imshow("Image",img);
+    waitKey(0);
 }
